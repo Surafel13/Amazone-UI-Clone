c3f165294f9725f94b18e2cbde8574944c30af8b
unknown <tesfayesurafel18@gmail.com>
2025-12-06 18:07:56 -1100

    Fotter Finished

--- Amazon-UI-Clone/src/Component/Footer/LowerFooter.jsx
+++ Amazon-UI-Clone/src/Component/Footer/LowerFooter.jsx
@@ -11,8 +11,61 @@ function LowerFooter() {
                 <div><span>$   USD - U.S Dollar</span></div>
                 <div><span>United State</span></div>
             </div>
+            <div className='Link_Wrapper_Head'>
+                <div className='Link_Wrapper'>
+                    <div className='text'>
+                        <div><a href=""><b>Amazon Music</b> <br /> Stream millions of songs</a></div>
+                        <div><a href=""><b>Amazon Business</b><br /> Everything For Your Business</a></div>
+                        <div><a href=""><b>IMDbPro</b> <br /> Get Info Entertainment Professionals Need</a></div>
+                    </div>
+                    <div className='text'>
+                        <div><a href=""><b>Amazon Ads</b><br /> Reach customers wherever they spend their time</a></div>
+                        <div><a href=""><b>AmazonGlobal</b><br /> Ship Orders Internationally</a></div>
+                        <div><a href=""><b>Kindle Direct</b><br /> Publishing Indie Digital & Print Publishing Made Easy</a></div>
+                        <div><a href=""><b>eero WiFi</b> <br /> Stream 4K Video in Every Room</a></div>
+                    </div>
+                    <div className='text'>
+                        <div><a href=""><b>Amazon Ads</b><br /> Reach customers wherever they spend their time</a></div>
+                        <div><a href=""><b>AmazonGlobal</b><br /> Ship Orders Internationally</a></div>
+                        <div><a href=""><b>Kindle Direct</b><br /> Publishing Indie Digital & Print Publishing Made Easy</a></div>
+                        <div><a href=""><b>eero WiFi</b> <br /> Stream 4K Video in Every Room</a></div>
+                    </div>
+                    <div className='text'>
+                        <div><a href=""><b>Amazon Ads</b><br /> Reach customers wherever they spend their time</a></div>
+                        <div><a href=""><b>AmazonGlobal</b><br /> Ship Orders Internationally</a></div>
+                        <div><a href=""><b>Kindle Direct</b><br /> Publishing Indie Digital & Print Publishing Made Easy</a></div>
+                        <div><a href=""><b>eero WiFi</b> <br /> Stream 4K Video in Every Room</a></div>
+                    </div>
+                    <div className='text'>
+                        <div><a href=""><b>Amazon Ads</b><br /> Reach customers wherever they spend their time</a></div>
+                        <div><a href=""><b>AmazonGlobal</b><br /> Ship Orders Internationally</a></div>
+                        <div><a href=""><b>Kindle Direct</b><br /> Publishing Indie Digital & Print Publishing Made Easy</a></div>
+                        <div><a href=""><b>eero WiFi</b> <br /> Stream 4K Video in Every Room</a></div>
+                    </div>
+                    <div className='text'>
+                        <div><a href=""><b>Amazon Ads</b><br /> Reach customers wherever they spend their time</a></div>
+                        <div><a href=""><b>AmazonGlobal</b><br /> Ship Orders Internationally</a></div>
+                        <div><a href=""><b>Kindle Direct</b><br /> Publishing Indie Digital & Print Publishing Made Easy</a></div>
+                        <div><a href=""><b>eero WiFi</b> <br /> Stream 4K Video in Every Room</a></div>
+                    </div>
+                    <div className='text'>
+                        <div><a href=""><b>Amazon Music</b> <br /> Stream millions of songs</a></div>
+                        <div><a href=""><b>Amazon Business</b><br /> Everything For Your Business</a></div>
+                        <div><a href=""><b>IMDbPro</b> <br /> Get Info Entertainment Professionals Need</a></div>
+                    </div>
+                </div>
+            </div>
+            <div className='bottom'>
+                <div><a href="">Condition of Use</a></div>
+                <div><a href="">Privacy Policy</a></div>
+                <div><a href="">Consumer Health Data Privacy Disclousre</a></div>
+                <div><a href="">Your adds Privacy Choice</a></div>
+            </div>
+            <div className='AllRight'>
+                <p>&copy;  1996-2025, Amazon.com, Inc. or its affiliates</p>
+            </div>
         </div>
-    
+
     )
 }
 
